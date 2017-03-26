5eaa082577c04d1d5927d3e8fb04326bd1479c65
FMcIntosh <frasermcintosh1@gmail.com>
2017-03-26 02:23:40 -0700

    Get svg map connected

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,32 @@
-// import map from '../assets/worldHigh.svg';
+import Head from 'next/head'
+
+import map from '../assets/worldHigh.svg';
+
+const mapSVG = map();
 
 export default () => (
   <div>
-    <p>Hey there, Welcome to Next.js!</p>
+  <Head>
+    <title>My page title</title>
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <style>{`
+    body, html {
+      margin: 0;
+      height: 100%;
+    }
+  `}</style>
+  </Head>
+    {map()}
+    <style>{`
+      svg {
+        width: 100%;
+        height: 100%;
+      }
+      div {
+        width: 100%;
+        height:100%;
+      }
+    `}</style>
   </div>
 
 )
